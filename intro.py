cb38852365e4f20818cb87c4344a0e4149837ea5
amishi <amishisingh135@gmail.com>
2024-09-28 22:03:04 +0530

    first commit in python

---
+++ intro.py
@@ -0,0 +1,17 @@
+print("amishi")
+print(12)
+print("summation is ",12+23)
+print("value for subtraction is ",12-23)
+print("amishi and darshit \n"*10)
+
+# arithmetic operators: +,-,*,/,%,**(power),//(floor div)
+print("add: ",10+12)
+print("sub: ",10-12)
+print("mul: ",10*12)
+print("div: ",10/12)
+print("remainder: ",23%7)
+print("power: ",2**3)
+print("div : ",11/2)
+print("floor div : ",11//2)
+
+
