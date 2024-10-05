e6ecd8212085617c1e40b537cc8c253e64a123bf
amishi <amishisingh135@gmail.com>
2024-10-05 22:21:00 +0530

    pyt

--- conditionalOprator.py
+++ conditionalOprator.py
@@ -1,13 +1,59 @@
-first_name = "Amishi "
-last_name = "singh"
-name = first_name + last_name
-age = 15
-school = "dps"
-amIIn10 = True
-print("my first name is",first_name)
-print("my name is",name)
-print("my age is",age)
-print("my school is",school)
-print( "I am in class 10",amIIn10)
-print(len(last_name))
-print(name[0:4])
+# first_name = "Amishi "
+# last_name = "singh"
+# name = first_name + last_name
+# age = 15
+# school = "dps"
+# amIIn10 = True
+# print("my first name is",first_name)
+# print("my name is",name)
+# print("my age is",age)
+# print("my school is",school)
+# print( "I am in class 10",amIIn10)
+# print(len(last_name))
+# print(name[2:4])
+# for z in range(3,90,3):
+#  print(z)
+#  h = 54
+#  print("the value of h is:",h)
+#  print("the type of data of h is :",type(h))
+# r = 65.87
+# print("the value of r is:",r)
+# print("the type of data of r is :",type(r))
+
+# n = False
+# print("the value of n is:",n)
+# print("the type of data of n is :",type(n))
+
+# k = "type of data"
+# print("the value of k is:",k)
+# print("the type of data of k is :",type(k))
+
+# i =input("enter value:")
+# print("the value of i:",i)
+# p =int(input("enter value:"))
+# print("the value of p:",p)
+# print("type of data :",type(p))
+
+# x=int(input("enter value:"))
+# print("the value of x:",x)
+# print("type of data :",type(x))
+# print(p*x)
+
+# import calendar
+# year =int(input("year:"))
+# print(calender.calender(year))
+
+
+p =float(input("enter value:"))
+
+
+
+x=float(input("enter value:"))
+
+
+print("multiplication of ",p,"and",x,"is",p*x)
+print("additon of ",p,"and",x,"is",p+x)
+print("substraction of",p,"and",x,"is",p-x)
+print("division of ",p,"and",x,"is",p/x)
+print("power of ",p,"as",x,"is",p**x)
+print("square root of",p,"is",x**0.5)
