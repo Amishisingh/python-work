10168e22ff79879f7ad8df51c08f2cf2e94caa5d
Amishisingh <amishisingh153@gmail.com>
2024-10-13 19:03:22 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -19,6 +19,16 @@ if answer == 'B':
 else:
     print("Incorrect")
     score = score - 5
+print("Entomology is the study of ...?")
+print("options: /nA.The origin and history of technical and scientific terms /nB.Behavior of human beings /nC.Insects")    
+answer = input("A/B/C:").upper()
+if answer == 'c':
+  print("correct")
+  score = score + 10
+else:
+    print("Incorrect")
+    score = score - 5
+
 
 print("------conclusion--------")
 print("hey",playerName,"Your score is ",score)
