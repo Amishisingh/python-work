b2342dd92ace804e68350caf2eb3d83f78ae5341
Amishisingh <amishisingh153@gmail.com>
2025-08-19 23:53:16 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 
-p =float(input("enter value:"))
+p=float(input("enter value:"))
 x=float(input("enter value:"))
 
 print("multiplication of ",p,"and",x,"is",p*x)
@@ -8,12 +8,15 @@ print("substraction of",p,"and",x,"is",p-x)
 print("division of ",p,"and",x,"is",p/x)
 print("power of ",p,"as",x,"is",p**x)
 print("square root of",p,"is",p**0.5)
+print("floor division of ",45,"by",10,"is",45/10=4.5,"its fd=",4,"as its the floor no. while 5 is the roof no.")
+print("for remainder of 2 no.s",45,6,45%6=3,"as 6*7=42","so 3 is left to reach 45")
 
 
 a=int(input("enter a number"))
 y=int(input("enter a number"))
 def add(a,y):
- def substract(a,y):
+ return a+y
+def substract(a,y):
     return a-y
 def multiply(a,y):
     return a*y
