c9d8255eae5801e940c7a74e416b37c50c34e543
Amishisingh <amishisingh153@gmail.com>
2025-11-26 19:35:26 +0530

    Add files via upload

---
+++ tkinter5.py
@@ -0,0 +1,10 @@
+def ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("when n is",n,"iteration=",iteration)
+
+ontime(12)
+ontime(40)
+ontime(29)
+print("\n with any 'n' time taken and iterations will increase")
