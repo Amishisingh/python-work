a1080deaf7ef81e4285a35e8968cac824274dbec
Amishisingh <amishisingh153@gmail.com>
2025-11-26 19:26:25 +0530

    Add files via upload

---
+++ tkinter4.py
@@ -0,0 +1,34 @@
+def printno(n):
+    iteration=0
+    print("the no. entered is",n)
+    iteration+=1
+    print("tot iterations in this code is",iteration,"\n")
+
+printno(12)
+printno(20)
+printno(29)
+print("\n with any 'n' time taken by our code wont change")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#rock,paper
+#calculator
+#rock,p..wigets
+#game
+#30
