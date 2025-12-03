4118b3bbcb8af4093513b7b121953b649a3ea4ac
Amishisingh <amishisingh153@gmail.com>
2025-12-03 19:55:50 +0530

    Update tkinter10.py

--- tkinter10.py
+++ tkinter10.py
@@ -1,5 +1,7 @@
 dfhnumber=int(input("enter a number"))
-for i in range(1,dfhnumber+2):
+for i in range(1,dfhnumber+1):
     if dfhnumber%i==0:
         print(i,"is a factor of ",dfhnumber)
-#
+#def print_factors(number):
+
+
