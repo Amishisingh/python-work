db02393d02eff96e625f42c6e1c03bd3121b092a
Amishisingh <amishisingh153@gmail.com>
2025-12-22 19:49:05 +0530

    Add files via upload

---
+++ tki14.py
@@ -0,0 +1,21 @@
+num1=10
+num2=4
+print("AND operator  num1 and num 2=", num1 & num2)
+#or operator
+print("\nnum1 | num 2=", num1 | num2)
+#not operator
+print("\n~num1 =", ~num1)
+#xor operator
+print("\nnum1 ^ num 2=", num1 ^ num2)
+num1=10
+num2=4
+#right shift on num1
+print("\nnum1 >> 1 =", num1 >> 1)
+#right shift on num2
+print("\nnum2 >> 1 =", num2 >> 1)
+num1=10
+num2=4
+#left shift on num1
+print("\nnum1 << 1 =", num1 << 1)
+#left shift on num2
+print("\nnum2 << 1 =", num2 << 1)
