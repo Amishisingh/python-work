19686566b5ff664886093b88ad78e88d4e933b3f
Amishisingh <amishisingh153@gmail.com>
2025-12-24 20:20:33 +0530

    Add files via upload

---
+++ tkinter16.py
@@ -0,0 +1,27 @@
+def totalSetBits(n): 
+#Count variable set as 0 
+    count = 0
+    # Right shift the number till we find first set bit
+    while (n):
+        if(n&1==1):
+            count+=1
+        n >>= 1       
+    return count
+
+# Program to find the number of zero bits and one bits present in a number
+# Function taking our number as input
+def numberOfBits(n):
+    ones = 0
+    zeros = 0
+    # while our number is greater than zero check last bit and right shift
+    while n:
+        # use AND operator to check if last bit is 1 or 0
+        if (n & 1) == 1:
+            ones += 1
+        else:
+            zeros += 1
+        # Right shift the number remove the last bit that we just checked above
+        n >>= 1
+    print("\nOnes =", ones, "\nZeros =", zeros)
+number = int(input("Enter your number : "))
+numberOfBits(number)
