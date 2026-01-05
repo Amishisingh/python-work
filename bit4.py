34206781a49d7b507bb899021dfd1a9d82c0b0e8
Amishisingh <amishisingh153@gmail.com>
2026-01-05 19:54:42 +0530

    Add files via upload

---
+++ bit4.py
@@ -0,0 +1,14 @@
+def swap1(a,b):
+    a=a^b
+    b=a^b
+    a=a^b
+    print(" After swapping a= ",a," b= ",b)
+
+def swap2(a,b):
+    a=(a&b)+(a|b)
+    b=a+(~b)+1
+    a=a+(~b)+1
+    print(" After swapping a= ",a," b= ",b)
+
+swap1(3,6)
+swap2(8,3)
